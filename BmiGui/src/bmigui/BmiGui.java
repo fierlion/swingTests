901661a3ccca3bce4bb7d7da07953080880b4130
rfoote <fierlion@gmail.com>
2014-11-19 12:38:59 -0800

    used gridLayout for panel and BorderLayout for frame.  Implemented actionListener on computeButton.

--- BmiGui/src/bmigui/BmiGui.java
+++ BmiGui/src/bmigui/BmiGui.java
@@ -5,17 +5,63 @@
  */
 package bmigui;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 /**
  *
- * @author rfoote
+ * @author rfoote/Reges/Stepp from "Building Java Programs"
  */
-public class BmiGui {
+public class BmiGui implements ActionListener {
+    private JFrame frame;
+    private JTextField heightField;
+    private JTextField weightField;
+    private JLabel bmiLabel;
+    private JButton computeButton;
+    
+    public BmiGui() {
+	heightField = new JTextField(5);
+	weightField = new JTextField(5);
+	bmiLabel = new JLabel("Type your height and weight");
+	computeButton = new JButton("Compute");
+	computeButton.addActionListener(this);
+	
+	//layout time...
+	JPanel north = new JPanel(new GridLayout(2,2));
+	north.add(new JLabel("Height:"));
+	north.add(heightField);
+	north.add(new JLabel("Weight:"));
+	north.add(weightField);
+	
+	frame = new JFrame("BMI");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLayout(new BorderLayout());
+	frame.add(north, BorderLayout.NORTH);
+	frame.add(bmiLabel, BorderLayout.CENTER);
+	frame.add(computeButton, BorderLayout.SOUTH);
+	
+	frame.pack();
+	frame.setVisible(true);
+	
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-	// TODO code application logic here
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	String heightText = heightField.getText();
+	//scrub and doubleify the input 
+	double height = Double.parseDouble(heightText);
+	String weightText = weightField.getText();
+	double weight = Double.parseDouble(weightText);
+	
+	//compute BMI and display it on screen
+	double bmi = weight / ((height * height) * 703);
+	bmiLabel.setText("BMI: " + bmi);
     }
     
+    
+    
+    
+
 }
---
+++ BmiGui/src/bmigui/BmiGuiTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bmigui;
+
+/**
+ *
+ * @author rfoote
+ */
+public class BmiGuiTest {
+    public static void main(String[] args) {
+	BmiGui bmiGuiTest = new BmiGui();
+    }    
+}
