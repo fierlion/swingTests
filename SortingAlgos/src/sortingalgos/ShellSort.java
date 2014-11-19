60c325a202c740b4200a7eb4523f333162c2f858
rfoote <fierlion@gmail.com>
2014-11-18 16:35:18 -0800

    added ShellSort implementation.

--- SortingAlgos/src/sortingalgos/ShellSort.java
+++ SortingAlgos/src/sortingalgos/ShellSort.java
@@ -10,5 +10,27 @@ package sortingalgos;
  * @author rfoote
  */
 public class ShellSort {
-    
+    public void shellsort(Integer [] arrIn) {
+	int inner;
+	int outer;
+	int temp;
+	
+	int interval = 1;
+	while (interval <= arrIn.length / 3) {
+	    interval = (interval * 3) + 1;
+	    while (interval > 0) {
+		for (outer = interval; outer < arrIn.length; outer++) {
+		    //check if arrIn[inner - interval] in index (inner - 
+		    //interval) is bigger than temp.
+		    temp = arrIn[outer];
+		    inner = outer;
+		    while (inner > interval - 1 && arrIn[inner - interval] >= temp){
+			arrIn[inner] = arrIn[inner - interval];
+		    }
+		    arrIn[inner] = temp;
+		}
+		interval = (interval - 1) / 3;
+	    }
+	}
+    }
 }
